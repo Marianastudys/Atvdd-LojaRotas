307b540e902ae64f274969eb56421fe57711e984
Mariana <mariastudys00@gmail.com>
2026-08-25 11:30:42 -0300

    mudando de any para Produtos

--- loja-frontend/src/app/produto.ts
+++ loja-frontend/src/app/produto.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Produtos } from './produtos/produtos';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class ProdutoService {
   constructor(private http: HttpClient) {}
 
   buscarProdutos() {
-    return this.http.get<any[]>(this.url);
+    return this.http.get<Produtos[]>(this.url);
   }
 
 }
